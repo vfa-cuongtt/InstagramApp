cf792c06a8f297deda8b2cd0e254502475aa0164
CuongTT <vfa.cuongtt@gmail.com>
2020-04-25 22:46:34 +0700

    commit create Tab bar controller

---
+++ InstargramApp/InstargramApp/Home/HomeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  HomeViewController.swift
+//  InstargramApp
+//
+//  Created by vfa on 4/25/20.
+//  Copyright © 2020 Tuan Cuong. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // change color
+        view.backgroundColor = UIColor.blue
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

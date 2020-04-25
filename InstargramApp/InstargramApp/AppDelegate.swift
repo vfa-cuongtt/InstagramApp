644e4eb0a1c84a5ef44a7be1a049d003a5053993
CuongTT <vfa.cuongtt@gmail.com>
2020-04-26 00:20:35 +0700

    add Navigation

--- InstargramApp/InstargramApp/AppDelegate.swift
+++ InstargramApp/InstargramApp/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Change color of tab bar's item
+        UITabBar.appearance().tintColor = .black
+        
         return true
     }
 
